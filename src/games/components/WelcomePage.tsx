e3ccb0bc669ca545e8da4ae94f2a1f2fc10917ab
charlie275691502 <charlie275691502@gmail.com>
2024-10-29 22:16:58 +1100

    Add progress bar images

--- src/games/components/WelcomePage.tsx
+++ src/games/components/WelcomePage.tsx
@@ -3,8 +3,13 @@ import { GameConnecting, GameLanding } from "../stage";
 import { useAppSelector } from "../../app/hooks";
 import { selectL1Account, selectL2Account } from "../../data/accountSlice";
 import { selectMemeList } from "../../data/puppy_party/properties";
+import WelcomePageProgressBar from "./WelcomePageProgressBar";
 
-const WelcomePage = () => {
+interface Props {
+  progress: number;
+}
+
+const WelcomePage = ({ progress }: Props) => {
   const memeList = useAppSelector(selectMemeList);
   const account = useAppSelector(selectL1Account);
   const l2account = useAppSelector(selectL2Account);
@@ -13,6 +18,12 @@ const WelcomePage = () => {
     <>
       {!account && <GameConnecting hint="connect wallet" />}
       {!l2account && account && <GameLanding memeList={memeList}></GameLanding>}
+      {progress > 0 ? (
+        <>
+          <div className="welcome-page-background-filter"></div>
+          <WelcomePageProgressBar progress={progress} />
+        </>
+      ) : null}
     </>
   );
 };
