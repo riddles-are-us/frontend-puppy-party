ef964d045386a92d0caaf7a14d588271a2d64ec0
charlie275691502 <charlie275691502@gmail.com>
2024-10-30 10:53:33 +1100

    fix gameplay status

--- src/games/components/Gameplay.tsx
+++ src/games/components/Gameplay.tsx
@@ -47,6 +47,30 @@ const Gameplay = () => {
   const [targetMemeRank, setTargetMemeRank] = useState(0);
   const [cooldown, setCooldown] = useState(false);
 
+  useEffect(() => {
+    const draw = (): void => {
+      const analyserInfo = audioSystem.play();
+      if (scenario.status === "play" && analyserInfo != null) {
+        const ratioArray = getBeat(analyserInfo!);
+        const progress = progressRef.current / 1000;
+        scenario.draw(ratioArray, {
+          progress,
+          l2account,
+          memeList,
+        });
+        scenario.step(ratioArray);
+      }
+    };
+
+    // Set the interval
+    const intervalId = setInterval(draw, 100); // 1000ms = 1 second
+
+    // Cleanup function to clear the interval when the component unmounts
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
   // Update the ref value whenever `progress` changes
   useEffect(() => {
     progressRef.current = progress;
