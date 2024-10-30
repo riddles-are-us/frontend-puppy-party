6cb6c52f97b0e37782854c9e55481b6ac5818dcb
charlie275691502 <charlie275691502@gmail.com>
2024-10-30 12:37:03 +1100

    Revert "try fix index exception"
    This reverts commit 79b3c26c24dbf58f79985999c6eb26115e4ed432.

--- src/games/stage.tsx
+++ src/games/stage.tsx
@@ -125,22 +125,17 @@ export function GameLanding(prop: { memeList: Array<any> }) {
       setMemeLayout((m) => {
         if (shuffled.length > 0) {
           const l = shuffled.pop();
-
-          if (indexedMemeList[l!.index]) {
-            const style = stageDivStyle(l!.value);
-            installedDiv.push(
-              <div
-                key={installedDiv.length}
-                style={style}
-                onClick={() => startGame(indexedMemeList[l!.index].index)}
-              ></div>
-            );
-            return {
-              divs: installedDiv,
-            };
-          } else {
-            return m;
-          }
+          const style = stageDivStyle(l!.value);
+          installedDiv.push(
+            <div
+              key={installedDiv.length}
+              style={style}
+              onClick={() => startGame(indexedMemeList[l!.index].index)}
+            ></div>
+          );
+          return {
+            divs: installedDiv,
+          };
         } else {
           return m;
         }
